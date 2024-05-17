c7fc265707ac37cc49cf4282724fcb16853ed15b
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-17 18:20:37 +0330

    [feat] create post server action validation

--- src/app/new-post/page.tsx
+++ src/app/new-post/page.tsx
@@ -1,17 +1,31 @@
-import FormSubmit from "@/components/form-submit";
+import PostForm from "@/components/post-form";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
 
 export default function NewPostPage() {
-  async function createPost(formData: FormData) {
+  async function createPost(state: { errors: string[] }, formData: FormData) {
     "use server";
+
     const title = formData.get("title") as string;
-    // const image = formData.get("image") as File;
+    const image = formData.get("image") as File;
     const content = formData.get("content") as string;
 
-    if (!(title && content)) {
-      return { message: "Something went wronge!" };
+    // --- START::validation
+    let errors = [];
+
+    if (!title || title.trim().length === 0) {
+      errors.push("Title is required!");
     }
+
+    if (!content || content.trim().length === 0) {
+      errors.push("Content is required!");
+    }
+    if (!image || image.size === 0) {
+      errors.push("Image is required!");
+    }
+
+    if (errors.length > 0) return { errors };
+    // --- END::validation
 
     await storePost({
       imageUrl: "",
@@ -22,32 +36,9 @@ export default function NewPostPage() {
 
     redirect("/feed");
   }
-
   return (
     <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows={5} />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
+      <PostForm action={createPost} />
     </>
   );
 }
