f8b4b570f8b7c123c06ad9990e5da24b2404f56e
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-18 14:45:30 +0330

    [feat] make Image component better
    Add priority for disable lazy loading and set width and height.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,13 @@ export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <Image src={logo} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          alt="Mobile phone with posts feed on it"
+          priority
+        />
       </Link>
       <nav>
         <ul>
